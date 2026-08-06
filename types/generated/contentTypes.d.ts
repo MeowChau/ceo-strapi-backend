4e9c0eacdf5caf334e6d22ea60d31c9e92dd983f
MeowChau <ChauNM.B23CC020@stu.ptit.edu.vn>
2026-08-07 02:58:32 +0700

    feat: change user relation to manyToOne for mentoring requests

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -592,7 +592,7 @@ export interface ApiMentoringRequestMentoringRequest
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     user: Schema.Attribute.Relation<
-      'oneToOne',
+      'manyToOne',
       'plugin::users-permissions.user'
     >;
   };
